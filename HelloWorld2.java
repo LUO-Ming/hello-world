c9792c134c114132582324bd3361d7acd276bce0
innox-luo <m.luo@innox-jp.com>
2017-04-07 16:14:26 +0900

    Hello World Eclipse EGit

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -4,6 +4,6 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World b22 !");
+        System.out.println("Hello World ! Eclipse EGit !!!");
     }
 }
